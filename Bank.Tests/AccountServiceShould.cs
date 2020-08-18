144b672b2b95111ff04ca425b8e96c40b42124ba
Tom <thomas.spencer@codurance.com>
2020-08-18 10:59:24 +0100

    Add AccountServiceTest

--- Bank.Tests/AccountServiceShould.cs
+++ Bank.Tests/AccountServiceShould.cs
@@ -24,5 +24,19 @@ namespace Bank.Tests
             Assert.AreEqual("Date | Amount | Balance\n" +
                             "18/08/2019 | 100.00 | 100.00", _accountService.PrintStatement());
         }
+
+        [Test]
+        public void store_multiple_deposits_on_same_day()
+        {
+            // Given
+            
+            // When
+            _accountService.Deposit(100);
+            _accountService.Deposit(100);
+            // Then
+            Assert.AreEqual("Date | Amount | Balance\n" +
+                            "18/08/2019 | 100.00 | 200.00\n" +
+                            "18/08/2019 | 100.00 | 100.00", _accountService.PrintStatement());
+        }
     }
 }
--- Bank/AccountService.cs
+++ Bank/AccountService.cs
@@ -1,19 +1,35 @@
 using System;
+using System.Collections.Generic;
+using System.Text;
 
 namespace Bank
 {
     public class AccountService
     {
 
+        List<int> deposits = new List<int>();
+        
         public const string Header = "Date | Amount | Balance";
         public String PrintStatement()
         {
+            StringBuilder result = new StringBuilder();
+
+            deposits.ForEach(d =>
+            {
+                result.Append("18/08/2019 | ");
+                result.Append(d);
+                result.Append();
+            });
+            
             return Header + "\n"
-                   + "18/08/2019 | 100.00 | 100.00";
+                          
+                          
+                          + "18/08/2019 | 100.00 | 100.00";
         }
 
         public void Deposit(int amount)
         {
+            deposits.Add(amount);
         }
 
         public void Withdraw(int amount)
